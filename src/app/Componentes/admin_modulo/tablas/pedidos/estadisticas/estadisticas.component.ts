ce8c697b2827241b6b5f774ae4182b4af17a847b
josue-balbontin <balbontinjosue@gmail.com>
2025-06-02 23:21:46 -0400

    .

--- src/app/Componentes/admin_modulo/tablas/pedidos/estadisticas/estadisticas.component.ts
+++ src/app/Componentes/admin_modulo/tablas/pedidos/estadisticas/estadisticas.component.ts
@@ -12,6 +12,7 @@ export class EstadisticasComponent implements OnInit {
   // Variables para estadísticas
   estadisticasCiudades: any[] = [];
   resumenSistema: any = {};
+  estadisticasAdicionales: any = {};
 
   constructor(private pedidosService: PedidosService) {}
 
@@ -23,6 +24,39 @@ export class EstadisticasComponent implements OnInit {
   actualizarVista(): void {
     this.estadisticasCiudades = this.pedidosService.obtenerEstadisticasPorCiudad();
     this.resumenSistema = this.pedidosService.obtenerResumenSistema();
+    this.calcularEstadisticasAdicionales();
+  }
+
+  // Calcular estadísticas adicionales
+  calcularEstadisticasAdicionales(): void {
+    if (this.estadisticasCiudades.length === 0) {
+      this.estadisticasAdicionales = {
+        ciudadMasPedidos: 'N/A',
+        ciudadMasIngresos: 'N/A',
+        promedioPedidos: '0.0'
+      };
+      return;
+    }
+
+    // Ciudad con más pedidos
+    const ciudadMasPedidos = this.estadisticasCiudades.reduce((prev, current) => 
+      (prev.total > current.total) ? prev : current
+    );
+
+    // Ciudad con más ingresos
+    const ciudadMasIngresos = this.estadisticasCiudades.reduce((prev, current) => 
+      (prev.ingresos > current.ingresos) ? prev : current
+    );
+
+    // Promedio de pedidos por ciudad
+    const totalPedidos = this.estadisticasCiudades.reduce((sum, ciudad) => sum + ciudad.total, 0);
+    const promedioPedidos = totalPedidos / this.estadisticasCiudades.length;
+
+    this.estadisticasAdicionales = {
+      ciudadMasPedidos: ciudadMasPedidos.ciudad,
+      ciudadMasIngresos: ciudadMasIngresos.ciudad,
+      promedioPedidos: promedioPedidos.toFixed(1)
+    };
   }
 
   // Método público para refrescar datos desde el componente padre
